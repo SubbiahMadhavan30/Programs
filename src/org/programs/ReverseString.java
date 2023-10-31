5c54d7843b454bccec7991219db4fa78d2777db8
SubbiahMadhavan30 <subbiahmadhavan30@gmail.com>
2023-10-31 13:02:42 +0530

    done

---
+++ src/org/programs/ReverseString.java
@@ -0,0 +1,23 @@
+package org.programs;
+
+import java.util.Scanner;
+
+public class ReverseString {
+public static void main(String args[]) {
+	/*String s = "maddy";
+	String rev ="";
+	int l = s.length();
+	for(int i= l-1;i>=0;i--) {
+	 rev  = rev + s.charAt(i);
+		
+		
+	}
+	System.out.println(rev);
+	
+}*/
+
+ 
+Scanner s = new Scanner(System.in);
+
+}
+}
